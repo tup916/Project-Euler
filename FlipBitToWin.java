765d0451700440632860ec1174b09a067cca8f7f
Tushita Patel <tushita.patel@usask.ca>
2017-01-17 12:45:38 -0600

    Update FlipBitToWin.java

--- FlipBitToWin.java
+++ FlipBitToWin.java
@@ -1,4 +1,10 @@
+/**
+You have an integer and you can flip exactly one bit from a 0 to a 1. Write code to find the longest sequence of 1s you could create. 
+Example:
+Input : 1775  (11011101111)
+Output: 8
 
+*/
 public class FlipBitToWin {
 
 	
